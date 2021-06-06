5781580d9f5d4e84d98ae22d3dd008473dd29873
Ritesh Jangra <riteshk2874@gmail.com>
2021-06-06 21:05:53 +0530

    mobile first part complete

---
+++ index.js
@@ -0,0 +1,8 @@
+$(".close-button").click(()=>{
+  $("#menu").toggleClass("hide")
+  $("#navbar").toggleClass("hide")
+})
+$(".menu-button").click(()=>{
+  $("#menu").toggleClass("hide")
+  $("#navbar").toggleClass("hide")
+})
